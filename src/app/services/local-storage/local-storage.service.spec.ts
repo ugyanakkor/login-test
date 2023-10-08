603c3c3d2b7858dbbf384618c9db2026c7e685d7
Kovács Tibor <tibor.kovacs@quest.com>
2023-10-08 16:28:29 +0200

    test(local-storage-service): add unit tests

--- src/app/services/local-storage/local-storage.service.spec.ts
+++ src/app/services/local-storage/local-storage.service.spec.ts
@@ -5,16 +5,16 @@ import { LocalStorageService } from "./local-storage.service";
 describe("LocalStorageService", () => {
   let service: LocalStorageService;
   const object = {
-    firstname: 'Barack',
-    lastName: 'Nemes',
-    email: 'test@gmail.com',
-    password: 'alma'
-  }
+    firstname: "Barack",
+    lastName: "Nemes",
+    email: "test@gmail.com",
+    password: "alma",
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({ providers: [LocalStorageService] });
     service = TestBed.inject(LocalStorageService);
-    localStorage.removeItem('test');
+    localStorage.removeItem("test");
   });
 
   it("should create LocalStorageService", () => {
@@ -22,24 +22,24 @@ describe("LocalStorageService", () => {
   });
 
   it("should set localstorage with test object", () => {
-    service.setLocalStorage('test', object);
-    const value = localStorage.getItem('test');
+    service.setLocalStorage("test", object);
+    const value = localStorage.getItem("test");
     expect(value ? JSON.parse(value) : null).toEqual(object);
   });
 
   it("should set localstorage with null", () => {
-    service.setLocalStorage('test', null);
-    const value = localStorage.getItem('test');
+    service.setLocalStorage("test", null);
+    const value = localStorage.getItem("test");
     expect(value ? JSON.parse(value) : null).toEqual(null);
   });
 
   it("should get localstorage with test object", () => {
-    localStorage.setItem('test', JSON.stringify(object));
-    expect(service.getLocalStorage('test')).toEqual(object);
+    localStorage.setItem("test", JSON.stringify(object));
+    expect(service.getLocalStorage("test")).toEqual(object);
   });
 
   it("should NOT get localstorage with test2 object", () => {
-    localStorage.setItem('test', JSON.stringify(object));
-    expect(service.getLocalStorage('test2')).not.toEqual(object);
+    localStorage.setItem("test", JSON.stringify(object));
+    expect(service.getLocalStorage("test2")).not.toEqual(object);
   });
 });
