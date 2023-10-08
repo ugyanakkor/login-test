d5f7b0ee25f60f4dddae697f69272c306b7356e0
Kovács Tibor <tibor.kovacs@quest.com>
2023-10-08 16:26:33 +0200

    test(local-storage-service): add unit tests

--- src/app/services/local-storage/local-storage.service.spec.ts
+++ src/app/services/local-storage/local-storage.service.spec.ts
@@ -4,13 +4,42 @@ import { LocalStorageService } from "./local-storage.service";
 
 describe("LocalStorageService", () => {
   let service: LocalStorageService;
+  const object = {
+    firstname: 'Barack',
+    lastName: 'Nemes',
+    email: 'test@gmail.com',
+    password: 'alma'
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({ providers: [LocalStorageService] });
     service = TestBed.inject(LocalStorageService);
+    localStorage.removeItem('test');
   });
 
-  it("should be created", () => {
+  it("should create LocalStorageService", () => {
     expect(service).toBeInstanceOf(LocalStorageService);
+  });
+
+  it("should set localstorage with test object", () => {
+    service.setLocalStorage('test', object);
+    const value = localStorage.getItem('test');
+    expect(value ? JSON.parse(value) : null).toEqual(object);
+  });
+
+  it("should set localstorage with null", () => {
+    service.setLocalStorage('test', null);
+    const value = localStorage.getItem('test');
+    expect(value ? JSON.parse(value) : null).toEqual(null);
+  });
+
+  it("should get localstorage with test object", () => {
+    localStorage.setItem('test', JSON.stringify(object));
+    expect(service.getLocalStorage('test')).toEqual(object);
+  });
+
+  it("should NOT get localstorage with test2 object", () => {
+    localStorage.setItem('test', JSON.stringify(object));
+    expect(service.getLocalStorage('test2')).not.toEqual(object);
   });
 });
